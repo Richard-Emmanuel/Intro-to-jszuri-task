bb6778e7c5ea9bece8f58c7232ea32436037ea6d
Richard-Emmanuel <richardnuelofficial@gmail.com>
2022-06-22 13:10:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var Name = "Richard Emmanuel";
+var Country = "Nigeria";
+var Height = "180cm";
+
+alert("My name is " + Name);
+alert("My country of origin is " + Country);
+alert("My Height is " + Height);
+
+console.log(Name);
+console.log(Country);
+console.log(Height);
